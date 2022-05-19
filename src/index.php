94a01e0e9570f56af6820e7d73c8549bfb7dfdf2
Sergey Baykovsky <baykovskys@gmail.com>
2022-05-19 21:06:46 +0300

    eps service logic with docker and env

---
+++ src/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once("EpsService.php");
+require_once("DotEnv.php");
+
+(new DotEnv(__DIR__ . '/.env'))->load();
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$service = new EpsService(getenv('APP_ENV'));
+
+if (empty($data)) {
+	return $service->response(405);
+}
+
+if(!$service->auth($data)){
+	return $service->response(401);
+}
+
+if(!$service->validate($data)){
+	return $service->response(400);
+}
+
+if(!$service->load($data)){
+	return $service->response(412);
+}
+
+if(!$service->send($data)){
+	return $service->response(500);
+}
+
+
